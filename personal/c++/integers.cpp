9113016fc58b0343d513823d84695d4512e307ca
achurs <achurs017@gmail.com>
2023-09-15 12:40:35 +0530

    outputszz

--- personal/c++/integers.cpp
+++ personal/c++/integers.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 int main(){
     int a = 12;
+    int b{13};
+    std::cout << b << std::endl;
     std::cout << a << std::endl;
+
+    std::cout << "size of b and a:"<< sizeof(b) << "," << sizeof(a) << std::endl;
+
     return 0;
 }
