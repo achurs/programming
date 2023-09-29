12691627b87bfe1d68c4f6a73e649ada6683fd07
achurs <achurs017@gmail.com>
2023-09-29 20:57:55 +0530

    tensura-font-first

---
+++ web-apps/tensura-font/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css'
+
+function App(){
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      text : ''
+    }
+  }
+  return (
+    <>
+      <h1 className='vitee'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
