e5d986e314782157d02b9ec1b15ee92200ef4ac9
achurs <achurs017@gmail.com>
2023-09-29 21:18:12 +0530

    font

--- web-apps/tensura-font/src/Font.jsx
+++ web-apps/tensura-font/src/Font.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Font.css'
 class Font extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.update = this.update.bind(this)
+    }
+    update(event) {
+        console.log("update is called");
+        this.setState({
+            text: event.target.value
+        })
+    }
     render() {
         return (
             <>
-                <h1 className='vitee'>Vite + React</h1>
+                <input type="text" placeholder='Enter text' onChange={this.update} value={this.state.input} />
+                <h1 className='Font'>{this.state.text}</h1>
             </>
         )
     }
