24a4c1a0c9ba7f28f4b9852c919848cb59a166ad
achurs <achurs017@gmail.com>
2023-10-01 01:13:53 +0530

    quote-jsx

---
+++ web-apps/quote-master/src/Quote.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Quote.css";
+class Quote extends React.Component {
+    render() {
+        return (
+            <div className="quote-box">
+                <h1>Quote</h1>
+            </div>
+        );
+    }
+}
+export default Quote;
