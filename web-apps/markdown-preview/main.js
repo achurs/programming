16595f816082e75b4ff0dbaddc9909fe1c3b6873
achurs <achurs017@gmail.com>
2023-10-07 22:44:14 +0530

    finished markdown-preview without css

--- web-apps/markdown-preview/main.js
+++ web-apps/markdown-preview/main.js
@@ -1,7 +1,25 @@
 import './style.css'
 
 $(document).ready(function () {
-  $("#markdown").css("color", "yellow");
+const defaultText = `
+# Header
+## Sub-Header
+[Link](https://example.com)
+\`inline code\`
+\`\`\`
+// Code block
+console.log("Hello, World!");
+\`\`\`
+- List item
+> Blockquote
+![Image](https://example.com/image.jpg)
+**Bolded text**
+`;
+  $("#editor").val(defaultText)
+  $("#preview").html(marked(defaultText))
+  $("#editor").keyup(function () {
+    $("#preview").html(marked($("#editor").val()))
+  })
 })
 
 setupCounter(document.querySelector('#counter'))
