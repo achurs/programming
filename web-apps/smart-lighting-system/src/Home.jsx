2ee23e853854798d6b86970e355883ecaa0b5d57
achurs <achurs017@gmail.com>
2023-11-30 18:19:53 +0530

    smart-lighting-system boilerplate

---
+++ web-apps/smart-lighting-system/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css'
+class Home extends React.Component {
+    render() {
+        return (
+            <>
+            <p>Hello</p>
+            </>
+        )
+    }
+}
+export default Home
