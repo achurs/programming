66d61537a5fb903a2d2bb425b6246cf9d19a62ef
achurs <achurs017@gmail.com>
2023-12-01 18:43:52 +0530

    cleaned-sls

--- web-apps/smart-lighting-system/src/Home.jsx
+++ web-apps/smart-lighting-system/src/Home.jsx
@@ -4,9 +4,33 @@ class Home extends React.Component {
     render() {
         return (
             <>
-            <p>Hello</p>
+            <Header />
+            <Main />
+            <Footer />
             </>
         )
     }
 }
 export default Home
+
+function Header() {
+    return (
+        <>
+        <p>Header</p>
+        </>
+    )
+}
+function Main() {
+    return (
+        <>
+        <p>Main</p>
+        </>
+    )
+}
+function Footer() {
+    return (
+        <>
+        <p>Footer</p>
+        </>
+    )
+}
