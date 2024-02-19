64382718bfa81c1eedaa3479ed7af5e1bd81177c
achurs <achurs017@gmail.com>
2024-02-19 14:57:14 +0530

    web-app(day-counter) beta version

---
+++ web-apps/day-counter/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import { useState } from 'react'
+import './daily-refesh.js'
+function setCookie(name,value) {
+  document.cookie = name + "=" + value;
+}
+function getCookie(name) {
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArray = cDecoded.split('; ');
+  let res = "";
+  cArray.forEach((element) => {
+    const c = element.split("=");
+    if (c[0] === name) {
+      console.log(c[1])
+      res = c[1];
+    }
+  })
+  return res;
+  }
+function daycounter(date1, date2) {
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return diffDays
+  
+}
+function App() {
+  let temp = getCookie("date");
+  const [date, setDate] = useState(temp);
+  let currentdate = new Date();
+  const [days, setDays] = useState(daycounter(new Date(date), currentdate));
+  return (
+    <>
+    <h1>Day Counter</h1>
+    <div>
+    <input type="date" placeholder='enter date' id='date-inputer' />
+    <button id='date-button' onClick={() => {
+      setDate(document.getElementById('date-inputer').value)
+      setCookie("date", document.getElementById('date-inputer').value)
+      setDays(daycounter(new Date(date), currentdate))
+    }}>Click</button>
+    </div>
+    {date}
+    <h1>{days} days</h1>
+    </>
+  )
+}
+
+export default App
---
+++ web-apps/day-counter/src/daily-refesh.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+const time = 24 * 60 * 60 * 1000;
+setInterval(function () {
+    //window reload
+    window.location.reload();
+},time)
